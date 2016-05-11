f9f3f991ca18bfb26752b22a58ece8582533bb06
Sandro Munda <sandro@munda.me>
2016-05-11 14:22:37 +0200

    WIP

--- services/value-stat-getter.js
+++ services/value-stat-getter.js
@@ -1,34 +1,48 @@
 'use strict';
+var P = require('bluebird');
 var OperatorValueParser = require('./operator-value-parser');
 
 function ValueStatGetter(model, params, opts) {
-  function getAggregateField() {
-    // jshint sub: true
-    return params['aggregate_field'] || 'id';
-  }
+  //function getAggregateField() {
+    //// jshint sub: true
+    //return params['aggregate_field'] || 'id';
+  //}
 
-  function getFilters() {
-    var filters = {};
+  //function getFilters() {
+    //var filters = {};
 
-    if (params.filters) {
-      params.filters.forEach(function (filter) {
-        filters[filter.field] = new OperatorValueParser(opts).perform(model,
-          filter.field, filter.value);
-      });
+    //if (params.filters) {
+      //params.filters.forEach(function (filter) {
+        //filters[filter.field] = new OperatorValueParser(opts).perform(model,
+          //filter.field, filter.value);
+      //});
+    //}
+
+    //return filters;
+  //}
+
+  function aggregate() {
+    switch (params.aggregate) {
+      case 'Count':
+        return 'COUNT(*)';
+      case 'Sum':
+        // jshint camelcase: false
+        return 'SUM(' + params.aggregate_field +')';
     }
-
-    return filters;
   }
 
   this.perform = function () {
-    return model
-      .aggregate(getAggregateField(), 'count', {
-        distinct: true,
-        where: getFilters()
-      })
-      .then(function (count) {
-        return { value: count };
+    var ds = model.dataSource;
+
+    var sql = 'SELECT ' + aggregate() + ' FROM ' +
+      model.settings.postgresql.table;
+
+    return new P(function (resolve, reject) {
+      ds.connector.query(sql, null, function (err, results) {
+        if (err) { return reject(err); }
+        resolve({ value: results[0][params.aggregate.toLowerCase()] });
       });
+    });
   };
 }
 
